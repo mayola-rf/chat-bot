44daf4914bca9c51a89f1734d33a5cd1db3c9781
mayolarf <mayolarf@gmail.com>
2026-03-10 14:47:05 +0530

    Prompt Template

--- src/main/java/com/playground/chatbot/controller/ChatController.java
+++ src/main/java/com/playground/chatbot/controller/ChatController.java
@@ -1,8 +1,14 @@
 package com.playground.chatbot.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ChatController {
@@ -29,6 +35,21 @@ public class ChatController {
                         .metadata("model", "gpt-4"))
                 .user(u -> u.text("How's the weather like in {state}")
                         .param("state", state))
+                .call()
+                .content();
+    }
+
+    @GetMapping("/ai/prompt")
+    String prompt(String country, String state) {
+        PromptTemplate promptTemplate = new PromptTemplate("How's the weather like in {state}");
+        Prompt userPrompt = promptTemplate.create(Map.of("state", state));
+
+
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate("You are an AI Assistant who knows weather across {country}");
+        Prompt systemPrompt = systemPromptTemplate.create(Map.of("country", country));
+
+        Prompt prompt = new Prompt(List.of(userPrompt.getUserMessage(), systemPrompt.getSystemMessage()));
+        return this.chatClient.prompt(prompt)
                 .call()
                 .content();
     }
