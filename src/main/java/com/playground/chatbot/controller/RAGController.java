1309e4456e7d9f2a85a823841b151c12b00b13ad
mayolarf <mayolarf@gmail.com>
2026-03-16 19:35:57 +0530

    feat RAG, Simple in-memory vector store with filter applied on the document metadata

--- src/main/java/com/playground/chatbot/controller/RAGController.java
+++ src/main/java/com/playground/chatbot/controller/RAGController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvi
 import org.springframework.ai.document.Document;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,25 +13,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ai/rag")
 public class RAGController {
 
     private ChatClient chatClient;
+    OllamaChatModel ollamaChatModel;
     private VectorStore vectorStore;
 
     RAGController(OllamaChatModel ollamaChatModel, OllamaEmbeddingModel ollamaEmbeddingModel) {
+        this.ollamaChatModel = ollamaChatModel;
         chatClient = ChatClient.builder(ollamaChatModel).build();
         vectorStore = SimpleVectorStore.builder(ollamaEmbeddingModel).build();
+        vectorStore.add(List.of(Document.builder()
+                        .text("Customer data should not be stored for more than 30 days")
+                        // country == 'UK' && year >= 2020 && isActive == true
+                        .metadata(Map.of("country", "UK"))
+                        .build(),
+                Document.builder()
+                        .text("Customer data should not be stored for more than 78 days")
+                        // country == 'UK' && year >= 2020 && isActive == true
+                        .metadata(Map.of("country", "India"))
+                        .build(),
+                Document.builder()
+                        .text("refund allowed within 30 days")
+                        .metadata(Map.of("country", "India"))
+                        .build()));
     }
 
     @GetMapping
     public String generateRAGResponse(String text) {
-        vectorStore.add(List.of(new Document("Customer data should not be stored for more than 30 days")));
         return this.chatClient.prompt(text)
                 .advisors(QuestionAnswerAdvisor.builder(vectorStore).build())
                 .user(text)
                 .call().content();
+    }
+
+    @GetMapping("/filters")
+    public String generateRAGResponseWithFilter(String text) {
+        ChatClient chatClient1 = ChatClient.builder(ollamaChatModel)
+                .defaultAdvisors(QuestionAnswerAdvisor.builder(vectorStore)
+//                        .searchRequest(SearchRequest.builder().similarityThreshold(0.8d).topK(5)
+//                                .filterExpression("country == 'India'")
+//                                .build())
+                        .build())
+                .build();
+        return chatClient1
+                .prompt(text)
+                .advisors(a -> a.param(QuestionAnswerAdvisor.FILTER_EXPRESSION, "country == 'India'"))
+//                .user(text)
+                .call()
+                .content();
     }
 }
